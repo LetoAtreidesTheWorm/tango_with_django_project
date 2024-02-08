9e35078a14a18bdbea28953df3bc05ed608301a1
Dylan Stair <2764165S@student.gla.ac.uk>
2024-02-08 23:08:38 +0000

    Chapter 8 Completed - all tests pass

--- rango/views.py
+++ rango/views.py
@@ -43,9 +43,9 @@ def add_page(request, category_name_slug):
 
     except Category.DoesNotExist:
         category = None
-
+# You cannot add a page to a Category that does not exist...
     if category is None:
-        return redirect('/rango/')
+        return redirect('/rango/home/')
     
     form = PageForm()
 
