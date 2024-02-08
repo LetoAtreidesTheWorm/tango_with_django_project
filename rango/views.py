7e33e5326cacbc717876597178ac30fdcf65ff6d
Dylan Stair <2764165S@student.gla.ac.uk>
2024-02-08 01:20:49 +0000

    Chapter 4 Completed - static and media images work but dont pass

--- rango/views.py
+++ rango/views.py
@@ -7,10 +7,17 @@ from rango.models import Category
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+    #return HttpResponse("<a href='/rango/about/'>About</a>")
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict['categories'] = category_list
+    return render(request, 'rango/index.html', context= context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    #return HttpResponse("BOMBOCLAAT <a href='/rango/home/'>Home</a>")
+    return render(request, 'rango/about.html', context= context_dict)
 
 
 
